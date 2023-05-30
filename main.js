63d51c452f11fecca59087693712287b65b45f21
CoderInProgress2 <119031303+CoderInProgress2@users.noreply.github.com>
2023-05-30 19:17:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-if ('serviceWorker' in navigator) {
-  var scope = location.pathname.replace(/\/[^\/]+$/, '/');
-  if (!scope.match(/__browserfs__/)) {
-    navigator.serviceWorker.register('sw.js', {scope})
-    .then(function(reg) {
-      reg.addEventListener('updatefound', function() {
-        var installingWorker = reg.installing;
-        console.log('A new service worker is being installed:',
-                    installingWorker);
-      });
-      // registration worked
-      console.log('Regristration succeeded. Scope is ' +
-                  reg.scope);
-    }).catch(function(error) {
-      // regristration failed
-      console.log('Regristration failed with ' + error);
+self.importScripts('https://cdn.jsdelivr.net/npm/browserfs');
+
+self.addEventListener('install', self.skipWaiting);
+
+self.addEventListener('activate', self.skipWaiting);
+
+self.addEventListener('fetch', function (event) {
+  let path = BrowserFS.BFSRequire('path');
+  let fs = new Promise(function(resolve, reject) {
+    BrowserFS.configure({ fs: 'IndexedDB', options: {} }, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(BrowserFS.BFSRequire('fs'));
+      }
     });
-  }
-}
+  });
+  event.respondWith(fs.then(function(fs) {
+    return new Promise(function(resolve, reject) {
+      function sendFile(path) {
+        fs.readFile(path, function(err, buffer) {
+          if (err) {
+            err.fn = 'readFile(' + path + ')';
+            return reject(err);
+          }
+          var ext = path.replace(/.*\./. '');
+          var mime = {
+            'html': 'text.html',
+            'json': 'application/json',
+            'js': 'application/javascript',
+            'css': 'text.css'
+          };
+          var headers - new Headers({
+            'Content-Type': mime[ext]
+          });
+          resolve(new Response(buffer, {headers}));
+        });
